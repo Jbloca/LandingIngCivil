bc99e1c27161fda8d2c84791dae9164865a48c6f
Jorge Balarezo <jbalarezocarden@gmail.com>
2025-04-04 12:30:03 -0500

    correcion rutas

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,7 @@ import { defineConfig } from 'astro/config';
 
 export default defineConfig({
   output: 'static',
-  base: '/LandingIngCivil/', // Cambia esto al nombre de tu repositorio
+  base: '/LandingIngCivil', // Cambia esto al nombre de tu repositorio
   build: {
     outDir: './dist',
   },
