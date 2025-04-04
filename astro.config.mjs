c8f9b0c071419001a7141f450bccabf1143757f4
Jorge Balarezo <jbalarezocarden@gmail.com>
2025-04-04 09:50:38 -0500

    correcion de rutas

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,7 @@ import { defineConfig } from 'astro/config';
 
 export default defineConfig({
   site: "https://jbloca.github.io/LandingIngCivil/",
-  base: "/LandingIngCivil",  // Asegúrate de que tenga la barra diagonal al inicio
+  base: "LandingIngCivil",
   output: 'static',
   build: {
     outDir: './dist',
