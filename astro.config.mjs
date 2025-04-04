eda917f00cc90a9377d430327430fc05993b2335
Jorge Balarezo <jbalarezocarden@gmail.com>
2025-04-04 10:39:43 -0500

    modificacion del astro.confi

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,6 @@
 import { defineConfig } from 'astro/config';
 
 export default defineConfig({
-  site: "https://jbloca.github.io/LandingIngCivil/",
-  base: "LandingIngCivil",
   output: 'static',
   build: {
     outDir: './dist',
